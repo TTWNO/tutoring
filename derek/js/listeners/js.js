a249350a4185db547f30172eea3464cfa6a3ceac
Tait Hoyem <tait@tait.tech>
2020-04-20 08:42:30 -0600

    Add listeners folder

---
+++ derek/js/listeners/js.js
@@ -0,0 +1,35 @@
+let theResult; 
+
+window.onload = function(){
+   theResult = document.getElementById("result");
+}
+
+
+
+//remove entire div
+
+function removeDiv(){
+       theResult.remove();
+};
+
+
+
+
+
+// append string onto text in div
+// change string to an input
+// add class to the div
+// add a sibling div below
+// go to parent and remove sib.
+
+
+
+
+
+
+
+
+{/* <div id="wrapper">
+            <div>Here is some text</div>
+            <div id="result">This is replaced by certain actions</div>
+        </div> */}
