ea51d1c8c16d05b69f1c3b0c5e5c98b4e75fd1ef
Tait Hoyem <811343@ACDM.DS.SAIT.CA>
2020-04-20 07:45:47 -0600

    Add Derek's files

---
+++ derek/js/dodgey-brakes/script.js
@@ -0,0 +1,35 @@
+var specials = querySelectorAll(".special")
+var politicals = querySelectorAll(".political")
+var politicals = querySelectorAll("div.political")
+
+var changetextsize = querySelector(".main")
+var firstp = querySelector ("p")
+
+var idwrapperchildren = querySelectorAll("#wrapper > *")
+
+
+var mainchildren = querySelectorAll(".main > .sub2")//all direct decendants of main
+// that has sub class 2.
+
+
+var g10 = 22;
+var grabMoney = getElementById("money");
+       
+grabMoney.classList.add("funny-text");
+grabMoney.classList.remove("funny-text");
+
+/* CSS FILE
+.funny-text {font-weight: bold; font-size: 24px; margin: 20px; background-color: 12px;}
+*/
+
+<span class="sub1 funny-text main-header article-title" id="money">Take the money and run!</span>
+
+
+
+// all children of elements with class main, which have the class sub2
+
+// get all elements of class main
+//   ... get all their children which have class sub2
+
+get element special class
+and first element that has class main
